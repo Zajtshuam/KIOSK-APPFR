65efa1615f2da27cdf406321117eb4957f2c37de
Zajtshuam Austin Xiong <zxiong@icstars.org>
2022-09-08 15:45:28 -0500

    HERE

---
+++ public/secondpage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Clock from './Clocker.js'
+
+function secondpage() {
+  return (
+
+    <div className="Clocker">
+      <Clock></Clock>
+      </div>
+  )
+}
+
+export default secondpage;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { useTransition, animated } from '@react-spring/web'
+import './App.css'
+import logo from './images/maskgroup.jpg'
+import photo from './images/PIXELS.jpg'
+import picture from './images/frameten.jpg'
+import Clock from './Clocker.js'
+import { BrowserRouter as Router, Routes, Link } from 'react-router-dom'
 
-function App() {
+const slides = [
+  logo, 
+  photo,
+  picture
+]
+
+export default function App() {
+  const [index, set] = useState(0)
+  const transitions = useTransition(index, {
+    key: index,
+    from: { opacity: 0},
+    enter: { opacity: 2},
+    leave: { opacity: -15},
+    config: { duration: 3000 },
+    onRest: (_a, _b, item) => {
+      if (index === item) {
+        set(state => (state + 1) % slides.length)
+      }
+    },
+    exitBeforeEnter: true,
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <Router>
+        <Link> 
+        {transitions((style, i) => (
+        <animated.div
+          className='bg'
+          style={{
+            ...style,
+            backgroundImage: `url(${slides[i]})`,
+          }}
+        />
+      ))}
+        </Link>
+        <Routes> 
+          <Route path = "/" element = ></Route>
+        </Routes>
+
+      
+    <div className="flex fill center">
+     
+      <div className="Clocker">
+      <Clock></Clock>
+      </div>
+      
+        </div>
+  
+       </Router>
     </div>
-  );
+   
+  
+  
+  )
 }
 
-export default App;
+
+
+
